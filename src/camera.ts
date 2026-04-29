53f88fc2c55b3da3576f8a51c58d8542445d8502
ochakuu8-sudo <ochakuu8@gmail.com>
2026-04-30 00:13:39 +0900

    Reduce camera scroll speed to normal gameplay rate

--- src/camera.ts
+++ src/camera.ts
@@ -3,6 +3,10 @@
  */
 import * as C from './constants';
 
+// constants.ts の SCROLL_SPEED_* は一時的にデバッグ用 10x の値になっているため、
+// 実際のカメラ移動だけ 0.1 倍に戻す。
+const SCROLL_SPEED_SCALE = 0.1;
+
 export class Camera {
   y = 0;
   /** 現在のスクロール速度 (px/s)。ゲーム側で燃料に応じて毎フレーム更新 */
@@ -11,7 +15,7 @@ export class Camera {
   lockY: number | null = null;
 
   update(dt: number) {
-    this.y += this.scrollSpeed * dt;
+    this.y += this.scrollSpeed * SCROLL_SPEED_SCALE * dt;
     if (this.lockY !== null && this.y >= this.lockY) {
       this.y = this.lockY;
     }
